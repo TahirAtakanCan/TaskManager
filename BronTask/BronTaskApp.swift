209afc6cd37529a0c55c92b61d2f1346e1911bf6
TahirAtakanCan <atkan4202@gmail.com>
2024-11-02 21:39:45 +0300

    first commit

---
+++ BronTask/BronTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  BronTaskApp.swift
+//  BronTask
+//
+//  Created by Tahir Atakan Can on 2.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BronTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
