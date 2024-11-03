3dad58291fd1968eaac9173eb223575fa975ba4e
TahirAtakanCan <atkan4202@gmail.com>
2024-11-03 12:59:34 +0300

    feat: TaskGroup defining data models

---
+++ BronTask/Model/TaskGroup.swift
@@ -0,0 +1,39 @@
+//
+//  TaskGroup.swift
+//  BronTask
+//
+//  Created by Tahir Atakan Can on 3.11.2024.
+//
+
+import Foundation
+
+
+struct TaskGroup: Identifiable {
+    let id = UUID()
+    var title: String
+    let creationDate: Date
+    var tasks: [Task]
+    
+    init(title: String, tasks: [Task] = []) {
+        self.title = title
+        self.creationDate = Date()
+        self.tasks = tasks
+    }
+    
+    static func example() -> TaskGroup {
+        let task1 = Task(title: "Satın Al")
+        let task2 = Task(title: "Projeyi Bitir")
+        let task3 = Task(title: "Dişçiyi Ara")
+        
+        var group = TaskGroup(title: "Yapılacaklar")
+        group.tasks = [task1, task2, task3]
+        return group
+    }
+    
+    static func exapmles() -> [TaskGroup] {
+        let group1 = TaskGroup.example()
+        let group2 = TaskGroup(title: "Yeni Liste")
+        return [group1, group2]
+    }
+    
+}
