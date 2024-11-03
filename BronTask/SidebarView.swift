c8646295ff2e6ec498216a7c666973b1dbfcb4fc
TahirAtakanCan <atkan4202@gmail.com>
2024-11-03 13:34:46 +0300

    feat: Sidebar and TaskList view added

---
+++ BronTask/SidebarView.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarView.swift
+//  BronTask
+//
+//  Created by Tahir Atakan Can on 3.11.2024.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SidebarView()
+}
---
+++ BronTask/TaskListView.swift
@@ -0,0 +1,18 @@
+//
+//  TaskListView.swift
+//  BronTask
+//
+//  Created by Tahir Atakan Can on 3.11.2024.
+//
+
+import SwiftUI
+
+struct TaskListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskListView()
+}
